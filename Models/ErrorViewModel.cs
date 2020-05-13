8fb9dee8bcfe71f7d8deca72c21ddf056a5455f0
Lyan Martins <lyan.martins3@gmail.com>
2020-05-12 23:51:42 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace char_creation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
