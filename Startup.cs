c6f4aed2b9b8ba7bd6f454fd25974b4d25940a67
Lyan Martins <lyan.martins3@gmail.com>
2020-05-14 00:16:17 -0300

    added entity and added database connection

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Models;
 
 namespace char_creation
 {
@@ -23,6 +25,9 @@ namespace char_creation
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connection = Configuration["ConexaoMySql:MySqlConnectionString"];
+            services.AddDbContext<DatabaseContext>(o => o.UseMySql(Configuration.GetConnectionString("MySqlConnectionString")));
+            
             services.AddControllersWithViews();
         }
 
